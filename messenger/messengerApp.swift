4fe4b85228079e3794d8f521059fc2e982658950
Miachał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-08-20 02:12:12 -0700

    initial commit

---
+++ messenger/messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerApp.swift
+//  messenger
+//
+//  Created by Michał Jażdżyk on 19/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
