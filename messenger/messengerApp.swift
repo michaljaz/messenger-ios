d321d4d6e6182b4442f6e5a25f7c7d5c27912c61
Miachał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-08-21 11:17:58 -0700

    working connection to firebase

--- messenger/messengerApp.swift
+++ messenger/messengerApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct messengerApp: App {
+    init(){
+        FirebaseApp.configure()
+        Auth.auth().createUser(withEmail:"xd@xd.pl",password:"aqq123")
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
